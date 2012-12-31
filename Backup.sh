d8be077534a84206ff07047e4fc42fb0a05eca3d
Shawn Fitzgerald <shawn.jess@gmail.com>
2012-12-30 19:35:03 -0500

    Added array for exclusions to the backup

--- Backup.sh
+++ Backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Author:		fitz24
+#Author:		Shawn (fitz24)
 #Date:			9-apr-2012
 #
 #
@@ -13,16 +13,19 @@
 #				-v verbose
 #				-i interactive 
 
-# Set the source directory to the home drive. Replace "~/" with "/home/homedrivename/*"
+# Set the source directory to the home drive. Replace "~/*" with "/home/homedrivename/*"
 Source="~/*"
 
 # Set the destination directory to the truecrypt directory "/media/truecrypt1" which is its default mount point
 Dest="/media/truecrypt1"
 
+# Set what directories to exclude from the backup
+ExcludeDirs=("~/ExcludeDir1" "~/ExcludeDir2")
+
+Backup="True"
+
 # Inform of what we are doing
 echo "Starting backup of $Source to $Dest"
-
-# Etc.
 echo "This may take a very long time"
 
 # Set the start time of the backup
@@ -32,18 +35,35 @@ echo "Started backup: $(date)" >> Backup.log
 for myFile in $Source
 
 	do
-		# Exclude the Backup directory because this is mounted to the remote backup drive and we have a recursive reference if we leave it in
-		if [ "$myFile" = "~/BackupDrive" ]; then
-		
-			echo "	no backup of " $myFile >> Backup.log
-			
-		else
-		
-			echo "	backup " $myFile >> Backup.log
-			#cp "$myFile" "$Dest" -r -u -p -v
-			
-		fi
-		
+
+		Backup="True"
+
+		#Loop the ExcludeDirs array
+		for myExclude in "${ExcludeDirs[@]}"
+
+			do
+
+				if [ "$myFile" = "$myExclude" ]; then
+
+					#Folder matched, do not backup this file/folder
+					Backup="False"
+
+					echo "	Skipping " $myFile >> Backup.log
+
+					break
+
+				fi
+				
+			done
+
+			#if the folder did not match in the exclude list, Backup will = true
+			if [ $Backup = "True" ]; then
+
+				echo "	Backup " $myFile >> Backup.log
+				cp "$myFile" "$Dest" -r -u -p -v
+
+			fi
+
 	done
 
 # Set the completion time of the backup
